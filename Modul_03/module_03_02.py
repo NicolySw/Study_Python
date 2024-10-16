d54509c986baa8b16e48c12807f26ff4becdd325
NicolySw <100844122+NicolySw@users.noreply.github.com>
2024-10-16 11:41:53 +0300

    выполнено задание 3_2

--- Modul_03/module_03_02.py
+++ Modul_03/module_03_02.py
@@ -7,7 +7,7 @@
 """
 
 
-def send_email(message, recipient, *, sender="university.help@gmail.ru"):
+def send_email(message, recipient, *, sender="university.help@gmail.com"):
     """
     Проверка на корректность e-mail отправителя и получателя.
     Проверка на отправку самому себе.
@@ -16,12 +16,24 @@ def send_email(message, recipient, *, sender="university.help@gmail.ru"):
     variants = ('.com', '.ru', '.net')
     if not (recipient.endswith(variants) and sender.endswith(variants) and '@' in recipient and '@' in sender):
         return f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}'
-    if sender == recipient:
-        return "Нельзя отправить письмо самому себе!"
-    if recipient == "university.help@gmail.com":
-        return f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}'
     else:
-        return f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}'
+        if sender == recipient:
+            return "Нельзя отправить письмо самому себе!"
+        else:
+            if sender == "university.help@gmail.com":
+                return f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}'
+            else:
+                return f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}'
 
 
-print(send_email("mes", "university.helpgmail.ru"))
+print(send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com'))
+print(send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com'))
+print(send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk'))
+print(send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru'))
+
+# Ожидаемый ответ:
+# Письмо успешно отправлено с адреса university.help@gmail.com на адрес vasyok1337@gmail.com
+# НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса urban.info@gmail.com на адрес urban.fan@mail.ru
+# Невозможно отправить письмо с адреса urban.teacher@mail.uk на адрес urban.student@mail.ru
+# Нельзя отправить письмо самому себе!
+
