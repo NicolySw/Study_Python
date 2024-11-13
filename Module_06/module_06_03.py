2107ef090ba1bd394d58fec7ab0f703337d20e51
NicolySw <100844122+NicolySw@users.noreply.github.com>
2024-11-13 14:17:33 +0300

    Выполнено домашнее задание 06_03

---
+++ Module_06/module_06_03.py
@@ -0,0 +1,74 @@
+import random
+
+
+class Animal:
+    live = True
+    sound = None
+    _DEGREE_OF_DANGER = 0
+    _cords = [0, 0, 0]
+
+    def __init__(self, speed):
+        self.speed = speed
+
+    def move(self, dx, dy, dz):
+        self._cords[0] += dx * self.speed
+        self._cords[1] += dy * self.speed
+        if dz * self.speed < 0:
+            print("It's too deep, i can't dive :(")
+        else:
+            self._cords[2] += dz * self.speed
+        return self._cords
+
+    def get_cords(self):
+        print(f'X: {self._cords[0]}, Y: {self._cords[1]}, Z: {self._cords[2]}')
+
+    def attack(self):
+        if self._DEGREE_OF_DANGER < 0:
+            print("Sorry, i'm peaceful :)")
+        else:
+            print("Be careful, i'm attacking you 0_0")
+
+    def speak(self):
+        print(self.sound)
+
+
+class Bird(Animal):
+    beak = True
+
+    def lay_eggs(self):
+        print(f"Here are(is) {random.randint(1, 4)} eggs for you")
+
+
+class AquaticAnimal(Animal):
+    _DEGREE_OF_DANGER = 3
+
+    def dive_in(self, dz):
+        self._cords[2] -= dz * abs(self.speed)//2
+
+
+class PoisonousAnimal(Animal):
+    _DEGREE_OF_DANGER = 8
+
+
+class Duckbill(Bird, AquaticAnimal, PoisonousAnimal):
+    sound = 'Click-click-click'
+
+    def __init__(self, speed):
+        super().__init__(speed)
+
+
+db = Duckbill(10)
+
+print(db.live)
+print(db.beak)
+
+db.speak()
+db.attack()
+
+db.move(1, 2, 3)
+db.get_cords()
+
+db.dive_in(6)
+db.get_cords()
+
+db.lay_eggs()
