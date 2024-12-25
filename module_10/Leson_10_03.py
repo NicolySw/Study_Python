9f1b132d3bf658b486c2a334787de7085e3e528c
NicolySw <100844122+NicolySw@users.noreply.github.com>
2024-12-25 13:11:55 +0300

    Выполнено задание 10_03

---
+++ module_10/Leson_10_03.py
@@ -0,0 +1,36 @@
+import threading
+
+counter = 0
+lock = threading.Lock()
+
+
+def increment(name):
+    global counter
+    with lock:
+        for i in range(10):
+            counter += 1
+            print(name, counter, lock.locked())
+
+
+def decrement(name):
+    global counter
+    try:
+        lock.acquire()
+        for i in range(10):
+            counter -= 1
+            print(name, counter,lock.locked())
+    except Exception:
+        pass
+    finally:
+        lock.release()
+
+
+thread1 = threading.Thread(target=increment, args=("thread1",))
+thread2 = threading.Thread(target=decrement, args=("thread2",))
+thread3 = threading.Thread(target=increment, args=("thread3",))
+thread4 = threading.Thread(target=decrement, args=("thread4",))
+
+thread1.start()
+thread3.start()
+thread2.start()
+thread4.start()
