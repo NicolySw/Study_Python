0919fb69d542126d71f0072349f860f57bec8a55
NicolySw <100844122+NicolySw@users.noreply.github.com>
2025-01-13 11:46:12 +0300

    Доделана практика по модулю 11

--- Module_11/Practice/main.py
+++ Module_11/Practice/main.py
@@ -1,19 +1,24 @@
 from PIL import Image, ImageDraw, ImageFont
 
 
-def new_photo(name):
-    image = Image.open(name)
-    w, h = image.size
-    return image.resize((w//2, h//2))
+class PostMaker:
+    def __init__(self, name_photo):
+        self.image = Image.open(name_photo)
+        self.w, self.h = self.image.size
+        self.image = self.image.resize((self.w // 2, self.h // 2))
+
+    def paste(self, name_photo):
+        pastel_image = Image.open(name_photo)
+        pastel_image = pastel_image.resize((pastel_image.size[0] // 2, pastel_image.size[1] // 2))
+        self.image.paste(pastel_image, (10, 10))
+
+    def upgrade(self, text):
+        draw = ImageDraw.Draw(self.image)
+        font = ImageFont.truetype('arial.ttf', 30)
+        draw.text((10, 10), text, font=font)
+        self.image.show()
 
 
-im = new_photo('foto.jpg')
-im_2 = new_photo('foto2.png')
-
-w, h = im.size
-im.paste(im_2, (w-100, h-100))
-draw = ImageDraw.Draw(im)
-font = ImageFont.truetype('arial.ttf', 50)
-draw.text((0, 0), text='Hello World', font=font, fill='red')
-im.show('MyPhoto')
-
+image = PostMaker('foto.jpg')
+image.paste('foto2.png')
+image.upgrade('Привет')
