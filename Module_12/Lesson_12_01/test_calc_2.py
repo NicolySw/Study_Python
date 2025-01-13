aa2b40de6695c3571080ebb636512315fb5ce39d
NicolySw <100844122+NicolySw@users.noreply.github.com>
2025-01-13 15:34:31 +0300

    2

--- Module_12/Lesson_12_01/test_calc_2.py
+++ Module_12/Lesson_12_01/test_calc_2.py
@@ -3,6 +3,20 @@ import unittest
 
 
 class CalkTest(unittest.TestCase):
+    def setUp(self):  # подключается перед каждым тестированием
+        print("setup")
+
+    @classmethod  # выполняется один раз при запуске
+    def setUpClass(cls):
+        print("MegaSetup")
+
+    def tearDown(self):  # запускается после любого теста
+        pass
+
+    @classmethod  # после всех тестов запускается
+    def tearDownClass(cls):
+        pass
+
     def test_add(self):
         """
         Test for add function in calculator
@@ -13,8 +27,9 @@ class CalkTest(unittest.TestCase):
     def test_sub(self):
         self.assertEqual(calk.sub(5, 3), 2)
 
+    def test_test(self):
+        self.assertIn("s", "ssdf")
+
 
 if __name__ == '__main__':
     unittest.main()
-
-
